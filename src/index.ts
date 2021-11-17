7b0bbe5f107717cb61e8774cf65f9c3c6e5a61cb
prothy <lukacsurgay@gmail.com>
2021-11-17 11:19:37 +0100

    feat: add typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Phaser from 'phaser'
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    transparent: true
+}
+
+new Phaser.Game(config)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
+import path from 'path'
+
 module.exports = {
+    entry: './src/index.ts',
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 }
